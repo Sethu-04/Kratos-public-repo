0e25702097dd2aa749e1e5ceab33bdfec0f77e32
Sethu <svrls@gmail.com>
2024-10-25 06:58:40 +0530

    add content

--- sethu.java
+++ sethu.java
@@ -1,5 +1,7 @@
-This is a sample file [1;5D[1;5D[1;5D[1;5D[1;5Dtext file used for github practice.
+This is a sample file text used for github practice.
 
 My self sethu from Chennai 
 I living with my family
 working as HR in yennes infotec 
+but right now i want to move into IT Field so i choose DevOps to learn in Staragile
+its all about me
